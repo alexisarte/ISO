0ceae4d09fb204c3482f90f00db025c36aacf585
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-09-17 13:36:34 -0300

    Fix

--- practica3/ejercicio14.sh
+++ practica3/ejercicio14.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
+# Recibe parámetros -a CADENA o -b CADENA
+# $1 nombre del directorio, $2 es -a o -b, $3 es CADENA
+# Ejemplo de Ejecucón bash -x ejercicio14RenombraArchivos /root/scripts/ejercicio14 -b zzzzzz
+
 if [ $# -ne 3 ]; then
+	echo "La cantidad de parametros es incorrecta"
 	exit 1
 fi
 
 # Verificar si el directorio existe
 if [ ! -d "$1" ]; then
-	echo "El directorio $1 no existe"
-    	exit 1
+	echo "El primer parametro no es un directorio existente"
+    	exit 2
+fi
+
+
+if [ -z "$3" ]; then
+	echo "El tercer parametro es nulo"
+	exit 3
 fi
 
 # Iterar sobre los archivos encontrados por find
 for file in $(find $1 -type f); do
 	name=$(basename "$file")
 	if [ "$2" == "-a" ]; then
-		mv "$file" "$(dirname "$file")/$name$3"     
+		mv "$file" "$1/$name$3"     
     	elif [ "$2" == "-b" ]; then	
-		mv "$file" "$(dirname "$file")/$3$name"     
+		mv "$file" "$1/$3$name"     
 	else
-		echo "Parámetro inválido"
-    fi
+		echo "El segundo parámetro debe ser -a o -b"
+    	fi
 done
