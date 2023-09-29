a072ab1e159134d045367aa3603d0d5308bbb34d
alexisarte <alexisrodriguezarteaga@email.com>
2023-09-29 15:02:37 -0300

    Fix

--- practica3/ejercicio19.sh
+++ practica3/ejercicio19.sh
@@ -2,14 +2,14 @@
 
 echo "MENU DE COMANDOS"
 
-select option in $(ls -p /home/alexis/ISO/practica3/ | grep -v /) salir 
+select option in $(ls | grep "\.sh") salir 
 do
 	case $option in
 		salir)
 			echo "Me alegra haber podido acrlararte esas dudas tan existenciales :)"
 			exit;;
 		*)
-			bash $script;;
+			bash $option;;
 	esac
 done
 
